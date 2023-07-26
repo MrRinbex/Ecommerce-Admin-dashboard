58458f5d23be0e2a0d4c8676fa02981f8a72902c
Léo Cheffi <97122274+MrRinbex@users.noreply.github.com>
2023-07-26 08:18:43 +0200

    navbar and footer design

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import "./footer.scss";
 
 const Footer = () => {
-  return <div className="footer">footer</div>;
+  return (
+    <div className="footer">
+      <span>E-Shop</span>
+      <span>© 2023 Leocheffi.com</span>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,28 @@
 import "./navbar.scss";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="logo.svg" alt="logo" />
+        <span>E-Shop</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="search" className="icon" />
+        <img src="/app.svg" alt="app" className="icon" />
+        <img src="/expand.svg" alt="expend" className="icon" />
+        <div className="notifications">
+          <img src="/notifications.svg" alt="notifications" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img src="https://leocheffi.com/images/leo.jpg" alt="" />
+          <span>Léo</span>
+        </div>
+        <img src="/settings.svg" alt="settings" className="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
