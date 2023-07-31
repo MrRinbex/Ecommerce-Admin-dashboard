0ade7f14b4a460a91ebd0038808b79590117dba1
Léo Cheffi <97122274+MrRinbex@users.noreply.github.com>
2023-07-31 07:38:08 +0200

    topBox with data

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,12 @@
+import TopBox from "../../components/topBox/topBox";
 import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
-      <div className="box box1">box1</div>
+      <div className="box box1">
+        <TopBox />
+      </div>
       <div className="box box2">box2</div>
       <div className="box box3">box3</div>
       <div className="box box4">box4</div>
