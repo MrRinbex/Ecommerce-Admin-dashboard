114aaff8b5c8a50efaf462c3d8456735885d8dbe
Léo Cheffi <97122274+MrRinbex@users.noreply.github.com>
2023-07-31 07:04:33 +0200

    home with grid template system & menu with data mapping

---
+++ src/data.ts
@@ -0,0 +1,116 @@
+export const menu = [
+    {
+        id:1,
+        title: "main",
+        listItem:[
+            {
+                id:1,
+                title: "Homepage",
+                url:"/",
+                icon:"home.svg"
+            },
+            {
+                id:2,
+                title: "Profile",
+                url:"/users/1",
+                icon:"user.svg"
+            },
+        ]
+    },
+    {
+        id:2,
+        title: "lists",
+        listItem:[
+            {
+                id:1,
+                title: "users",
+                url:"/users",
+                icon:"user.svg"
+            },
+            {
+                id:2,
+                title: "Products",
+                url:"/products",
+                icon:"product.svg"
+            },
+            {
+                id:3,
+                title: "Orders",
+                url:"/orders",
+                icon:"order.svg"
+            },
+            {
+                id:4,
+                title: "Posts",
+                url:"/posts",
+                icon:"post2.svg"
+            },
+        ]
+    },
+    {
+        id:3,
+        title: "general",
+        listItem:[
+            {
+                id:1,
+                title: "Elements",
+                url:"/",
+                icon:"element.svg"
+            },
+            {
+                id:2,
+                title: "Notes",
+                url:"/",
+                icon:"note.svg"
+            },
+            {
+                id:3,
+                title: "Forms",
+                url:"/",
+                icon:"form.svg"
+            },
+            {
+                id:4,
+                title: "Calender",
+                url:"/",
+                icon:"calendar.svg"
+            },
+        ]
+    },
+    {
+        id:4,
+        title: "maintenance",
+        listItem:[
+            {
+                id:1,
+                title: "Settings",
+                url:"/",
+                icon:"setting.svg"
+            },
+            {
+                id:2,
+                title: "Backup",
+                url:"/",
+                icon:"backup.svg"
+            },
+        ]
+    },
+    {
+        id:5,
+        title: "analytics",
+        listItem:[
+            {
+                id:1,
+                title: "Charts",
+                url:"/",
+                icon:"chart.svg"
+            },
+            {
+                id:2,
+                title: "Logs",
+                url:"/",
+                icon:"log.svg"
+            },
+        ]
+    },
+]
